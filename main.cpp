77651781c43db640fbe2d1c5d4d33ede1b9c1d05
Turupawn <ahmed.hn.43@gmail.com>
2015-02-19 17:54:59 -0600

    primer commit

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include "Evaluador.h"
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <vector>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+//Devuele la suma de todos los elementos de mi_pila (dado) y mi_cola (dado)
+float sumarElementos(stack<float> mi_pila, queue<float> mi_cola)
+{
+    return -1.0f;
+}
+
+//Devuelve true si mi_vector (dado) tiene valores repetidos
+bool existenRepetidos(vector<string> mi_vector)
+{
+    return false;
+}
+
+//Devuelve la cantidad de elementos en edades (dado) que son mayores o iguales que 18
+int contarMayoresDeEdad(list<int>edades)
+{
+    return -1;
+}
+
+//Devolver el un mapa con los siguientes valores dados en la tabla
+//   LLave      |   Valor
+//----------------------------
+//  "Lolo"      |   98923212
+//  "Lola""     |   92123243
+//  "Rufo"      |   93212322
+//  "Punpun"    |   99321273
+//  "Ponpon"    |   92133243
+map<string,int> obtenerAgenda()
+{
+    map<string,int> agenda;
+    return agenda;
+}
+
+//Devuelve el numero menor en mi_set (dado)
+double obtenerMenor(set<double> mi_set)
+{
+    return -1.0;
+}
+
+int main ()
+{
+
+    //Funcion evaluadora
+    evaluar();
+    return 1;
+}
